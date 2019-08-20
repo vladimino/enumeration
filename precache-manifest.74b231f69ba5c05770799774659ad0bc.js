13d2abde75263f2695ec9083d17bb0b368638904
Vlad Bilokur <vladimino@gmail.com>
2019-08-20 15:09:54 +0200

    Updates

---
+++ precache-manifest.74b231f69ba5c05770799774659ad0bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00d09c43c9fd5a0541052b261b165330",
+    "url": "/enumeration/index.html"
+  },
+  {
+    "revision": "9980307c828d50855918",
+    "url": "/enumeration/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f76810aae0f845c25645",
+    "url": "/enumeration/static/js/2.8f9c5b62.chunk.js"
+  },
+  {
+    "revision": "9980307c828d50855918",
+    "url": "/enumeration/static/js/main.7a0b4ec0.chunk.js"
+  },
+  {
+    "revision": "35f447758f33a1139c34",
+    "url": "/enumeration/static/js/runtime~main.b756382c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/enumeration/static/media/logo.5d5d9eef.svg"
+  }
+]);
